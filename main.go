468f481ed352a0da3d2f6d1fd620a5cc2466b64f
daikiku10 <soccerdk19.910@gmail.com>
2023-02-25 22:39:52 +0900

    Ginのインストール

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("こんにちは！！！")
+	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/*.html")
+
+	router.GET("/", func(ctx *gin.Context){
+		ctx.HTML(200, "index.html", gin.H{})
+	})
+
+	router.Run()
 }
