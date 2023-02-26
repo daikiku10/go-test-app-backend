d48a5f203584504138e8fd74c0d9863f75297c9a
daikiku10 <soccerdk19.910@gmail.com>
2023-02-26 13:55:04 +0900

    update:CRUD処理の実装

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -9,16 +10,56 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+type User struct {
+	gorm.Model
+	Name string
+	Email string
+}
+
 func main() {
 	db := sqlConnect()
+	db.AutoMigrate(&User{})
 	defer db.Close()
 	
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
 
 	router.GET("/", func(ctx *gin.Context){
-		ctx.HTML(200, "index.html", gin.H{})
+		db := sqlConnect()
+		var users []User
+		db.Order("created_at asc").Find(&users)
+		defer db.Close()
+
+		ctx.HTML(200, "index.html", gin.H{
+			"users": users,
+		})
 	})
+
+	router.POST("/new", func(ctx *gin.Context) {
+		db := sqlConnect()
+		name := ctx.PostForm("name")
+		email := ctx.PostForm("email")
+		fmt.Println("作成ユーザーは" + name + ":" + email)
+		db.Create(&User{Name: name, Email: email})
+		defer db.Close()
+
+		ctx.Redirect(302, "/")
+	})
+
+	router.POST("/delete/:id", func(ctx *gin.Context) {
+    db := sqlConnect()
+    n := ctx.Param("id")
+    id, err := strconv.Atoi(n)
+    if err != nil {
+      panic("id is not a number")
+    }
+    var user User
+    db.First(&user, id)
+    db.Delete(&user)
+    defer db.Close()
+
+    ctx.Redirect(302, "/")
+  })
 
 	router.Run()
 }
