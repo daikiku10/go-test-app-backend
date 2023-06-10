a583d87ade823b004ebfd3e67732b1a018d7e7c3
daikiku10 <soccerdk19.910@gmail.com>
2023-06-10 16:50:05 +0900

    update: echoのルーターを使う

--- main.go
+++ main.go
@@ -4,15 +4,38 @@ import (
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
-func rootHandler(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello from Go Echo")
+type res struct {
+	TestInt    int    `json:"testInt"`
+	TestString string `json:"testString"`
 }
 
 func main() {
+	// Echoインスタンスの作成
 	e := echo.New()
-	e.GET("/", rootHandler)
-	e.Logger.Fatal(e.Start(":8080"))
+
+	// ルート指定
+	e.GET("/test", getHandler)
+	e.POST("/test", postHandler)
+
+	// サーバ起動
+	e.Start(":8080")
+}
+
+func getHandler(c echo.Context) error {
+	response := res{
+		TestInt:    21,
+		TestString: "getTestです。",
+	}
+	return c.JSON(http.StatusOK, response)
+}
+
+func postHandler(c echo.Context) error {
+	response := res{
+		TestInt:    21,
+		TestString: "postTestです。",
+	}
+	return c.JSON(http.StatusCreated, response)
 }
