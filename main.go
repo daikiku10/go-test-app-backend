9adbf311618c3f36a8cfa3c9a02669646078b668
daikiku10 <soccerdk19.910@gmail.com>
2023-06-28 23:23:15 +0900

    update: main.goの異常プログラム終了処理

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-	"github.com/daikiku10/go-test-app-backend/route"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	e := route.Init()
+	// e := route.Init()
 
-	// サーバ起動
-	// e.Start(":8080")
-	e.Logger.Fatal(e.Start(":8080"))
+	// // サーバ起動
+	// // e.Start(":8080")
+	// e.Logger.Fatal(e.Start(":8080"))
+
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminated server: %v", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) error {
+	fmt.Println("aaa")
+
+	return nil
 }
