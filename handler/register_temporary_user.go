1db85222b5fc984933e178265ab659d0df85b9ba
daikiku10 <soccerdk19.910@gmail.com>
2023-07-23 21:53:24 +0900

    update:ユーザー仮登録のクライアントリクエストを受け取る

--- handler/register_temporary_user.go
+++ handler/register_temporary_user.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,7 +15,31 @@ func NewRegisterTemporaryUser(rtu RegisterTemporaryUserService) *RegisterTempora
 	return &RegisterTemporaryUser{Service: rtu}
 }
 
+// ユーザー仮登録ハンドラー
+//
+// @param ctx ginContext
 func (rtu *RegisterTemporaryUser) ServerHTTP(ctx *gin.Context) {
 	fmt.Println("ハンドラー層：仮ユーザー登録API")
+	// エラータイトル
+	const errorTitle = "ユーザー仮登録エラー"
+	// クライアント リクエスト情報
+	var input struct {
+		FirstName      string `json:"firstName"`
+		FirstNameKana  string `json:"firstNameKana"`
+		FamilyName     string `json:"FamilyName"`
+		FamilyNameKana string `json:"FamilyNameKana"`
+		Password       string `json:"password"`
+		Email          string `json:"email"`
+	}
+
+	fmt.Printf("%+v", &input)
+
+	// クライアントから正しいパラメータでデータが送られていない
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		APIErrorResponse(ctx, http.StatusBadRequest, errorTitle, err.Error())
+		return
+	}
+
+	fmt.Printf("%+v", &input)
 	rtu.Service.RegisterTemporaryUser()
 }
