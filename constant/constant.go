bec9182149386e599a7a15581972d97048fa428e
daikiku10 <soccerdk19.910@gmail.com>
2023-09-13 21:51:46 +0900

    update:定数の追加

--- constant/constant.go
+++ constant/constant.go
@@ -5,6 +5,12 @@ const (
 	ConfirmCodeLength int = 4
 	// 確認コードの有効期限
 	ConfirmCodeExpiration_m = 60
+	// キャッシュ側のトークンの有効期限
+	TokenExpiration_m int = 60
+	// JWT側の最大有効期限（連続操作この指定時間経てば期限が切れる）
+	MaxTokenExpiration_m int = 3600
+	// ランダムパスワードの桁数
+	RandomPasswordLength int = 12
 
 	// 名字・名前の最大文字数
 	UserNameMaxLength int = 50
