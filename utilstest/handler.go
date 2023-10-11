dd5c18e1209138048cf9664f787999d33c06ae41
daikiku10 <soccerdk19.910@gmail.com>
2023-10-11 21:49:32 +0900

    update:レスポンス検証の共通関数の作成

--- utilstest/handler.go
+++ utilstest/handler.go
@@ -1,9 +1,41 @@
 package utilstest
 
 import (
+	"io"
+	"net/http"
 	"os"
 	"testing"
 )
+
+// テスト用
+// アサーション関数
+// @params
+// t テストユーティリティ
+// got 実際のレスポンス
+// status  wantレスポンスステータスコード
+// body wantレスポンスボディ
+func AssertResponse(t *testing.T, got *http.Response, status int, body []byte) {
+	// テスト関数がエラーの発生源であることを示すために指定する
+	t.Helper()
+
+	t.Cleanup(func() { _ = got.Body.Close() })
+	// データを読み取りバイトスライスで返す
+	gb, err := io.ReadAll(got.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got.StatusCode != status {
+		t.Fatalf("want status %d, but got %d, body: %q", status, got.StatusCode, gb)
+	}
+
+	// 期待としても実体としてもレスポンスボディがない場合
+	if len(gb) == 0 && len(body) == 0 {
+		// AssertJSONを呼ぶ必要はない。
+		return
+	}
+	// AssertJSON(t, body, gb)
+}
 
 // テスト用
 // JSONファイルの読み込み
