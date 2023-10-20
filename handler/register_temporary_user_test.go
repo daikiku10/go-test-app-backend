73762f0f68710772212b456ec618742248739add
daikiku10 <soccerdk19.910@gmail.com>
2023-10-20 12:44:06 +0900

    update:仮登録APIのハンドラーのテストコード

--- handler/register_temporary_user_test.go
+++ handler/register_temporary_user_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"errors"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -26,38 +25,38 @@ func TestRegisterTemporaryUser(t *testing.T) {
 		want    want
 	}{
 		"正しいリクエストの時は201となる": {
-			reqFile: "reqTest1",
+			reqFile: "testdata/register_temporary_user/201_req_json_golden",
 			want: want{
 				status:  http.StatusCreated,
-				resFile: "resTest1",
+				resFile: "testdata/register_temporary_user/201_res_json_golden",
 			},
 		},
 		"正しくないパラメータの時は400となる": {
-			reqFile: "reqTest2",
+			reqFile: "testdata/register_temporary_user/400_1_req_json_golden",
 			want: want{
 				status:  http.StatusBadRequest,
-				resFile: "resTest2",
+				resFile: "testdata/register_temporary_user/400_1_res_json_golden",
 			},
 		},
 		"バリデーションエラーの時は400となる": {
-			reqFile: "reqTest3",
+			reqFile: "testdata/register_temporary_user/400_2_req_json_golden",
 			want: want{
 				status:  http.StatusBadRequest,
-				resFile: "resTest3",
+				resFile: "testdata/register_temporary_user/400_2_res_json_golden",
 			},
 		},
 		"既にユーザーが存在する時は409となる": {
-			reqFile: "reqTest4",
+			reqFile: "testdata/register_temporary_user/409_req_json_golden",
 			want: want{
 				status:  http.StatusConflict,
-				resFile: "resTest4",
+				resFile: "testdata/register_temporary_user/409_res_json_golden",
 			},
 		},
 		"予期せぬエラー時は500となる": {
-			reqFile: "reqTest5",
+			reqFile: "testdata/register_temporary_user/500_req_json_golden",
 			want: want{
 				status:  http.StatusInternalServerError,
-				resFile: "resTest5",
+				resFile: "testdata/register_temporary_user/500_res_json_golden",
 			},
 		},
 	}
@@ -65,6 +64,7 @@ func TestRegisterTemporaryUser(t *testing.T) {
 	for k, test := range tests {
 
 		t.Run(k, func(t *testing.T) {
+			// TODO: パラレル
 			// サービス層のモック定義
 			moq := &RegisterTemporaryUserServiceMock{}
 			moq.RegisterTemporaryUserFunc = func(ctx context.Context, input service.ServiceRegisterTemporaryUserInput) (string, error) {
@@ -80,19 +80,24 @@ func TestRegisterTemporaryUser(t *testing.T) {
 
 			// テストデータの挿入する
 			// gin Context の生成
-			c, _ := gin.CreateTestContext(httptest.NewRecorder())
+			w := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(w)
 			// リクエストの生成
 			req, _ := http.NewRequest("POST", "/temporary_user", bytes.NewReader(utilstest.LoadFile(t, test.reqFile)))
 			// リクエスト情報をコンテキストに入れる
 			c.Request = req
 
 			// リクエストの送信
+			rtu := RegisterTemporaryUser{
+				Service: moq,
+			}
+			rtu.ServerHTTP(c)
 
 			// レスポンス
+			res := w.Result()
 
 			// 検証
+			utilstest.AssertResponse(t, res, test.want.status, utilstest.LoadFile(t, test.want.resFile))
 		})
-		fmt.Println(k)
-		fmt.Println(test)
 	}
 }
