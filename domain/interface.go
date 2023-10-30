a060ef436de524ff976c33230ce1f17b5c5509a0
daikiku10 <soccerdk19.910@gmail.com>
2023-10-30 20:54:19 +0900

    update:ユーザー情報取得APIの実装

--- domain/interface.go
+++ domain/interface.go
@@ -15,6 +15,7 @@ type UserRepo interface {
 	// SQLBoiler
 	RegisterUserBoiler(ctx context.Context, u *models.User, db *sqlx.DB) error
 	GetAllUsers(ctx context.Context, db *sqlx.DB) ([]*models.User, error)
+	GetUserByID(ctx context.Context, db *sqlx.DB, tuID string) (*models.User, error)
 	// SQLBoilerではない
 	RegisterUser(ctx context.Context, db repository.Execer, u *model.User) error
 	FindUserByEmail(ctx context.Context, db repository.Queryer, email string) (model.User, error)
--- service/get_user.go
+++ service/get_user.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/daikiku10/go-test-app-backend/domain"
 	"github.com/gin-gonic/gin"
@@ -21,7 +22,12 @@ type UserByID struct {
 }
 
 type GetUserResponse struct {
-	User UserByID `json:"user"`
+	ID             int    `json:"id"`
+	FirstName      string `json:"firstName"`
+	FirstNameKana  string `json:"firstNameKana"`
+	FamilyName     string `json:"FamilyName"`
+	FamilyNameKana string `json:"FamilyNameKana"`
+	MailAddress    string `json:"mailAddress"`
 }
 
 func NewGetUser(db *sqlx.DB, repo domain.UserRepo) *GetUser {
@@ -30,5 +36,27 @@ func NewGetUser(db *sqlx.DB, repo domain.UserRepo) *GetUser {
 
 // ユーザー情報取得
 func (c *GetUser) GetUser(ctx *gin.Context) {
+	// クエリパラメータの取得
+	uID := ctx.Query("userId")
+	if uID == "" {
+		ctx.JSON(400, "不正なパラメーターです。")
+	}
+
+	u, err := c.Repo.GetUserByID(ctx, c.DB, uID)
+	if err != nil {
+		ctx.JSON(400, err.Error())
+		return
+	}
+
+	// レスポンス作成
+	res := GetUserResponse{
+		ID:             int(u.ID),
+		FirstName:      u.FirstName,
+		FirstNameKana:  u.FirstNameKana,
+		FamilyName:     u.FamilyName,
+		FamilyNameKana: u.FamilyNameKana,
+		MailAddress:    u.Email,
+	}
+	ctx.JSON(http.StatusOK, res)
 	fmt.Println("ユーザー情報取得成功です！")
 }
